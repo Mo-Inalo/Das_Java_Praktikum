64740603bdaa51d52db45f80b6aca5903d2b758a
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2023-01-21 23:52:28 +0100

    aufgaben bis 7  umbenannt

--- 7_rectangles/Rectangles.java
+++ 7_rectangles/Rectangles.java
@@ -1,33 +1,7 @@
-/* Das Java-Praktikum, dpunkt Verlag 2008, ISBN 978-3-89864-513-3
- * Aufgabe: Rechtecke
- * Entwickelt mit: Sun Java 6 SE Development Kit
- */
-package rectangles;
 
-/**
- * Klassenrahmen für Programm Rechtecke.
- *
- * @author Klaus Köhler, koehler@hm.edu
- * @author Reinhard Schiedermeier, rs@cs.hm.edu
- * @version 27.06.2008
- */
 class Rectangles {
 
-    /**
-     * Klassifiziert die gegenseitige Lage von zwei achsenparallelen Rechtecken in der Ebene.
-     * Gibt eines der folgenden Wörter aus:
-     * disjoint Der Durchschnitt der beiden Rechtecke ist leer.
-     * same     Lage und Größe beider Rechtecke ist gleich.
-     * contained Der Durchschnitt der beiden Rechtecke fällt mit genau einem der Rechtecke zusammen.
-     *          Alle Punkte eines der Rechtecke sind auch im anderen enthalten, aber nicht umgekehrt.
-     * aligned  Der Durchschnitt der beiden Rechtecke ist eine Linie.
-     *          Alle gemeinsamen Punkte liegen auf einer Linie mit Länge > 0.
-     * touching Der Durchschnitt der beiden Rechtecke ist ein Punkt.
-     *          Es gibt genau einen gemeinsamen Punkt.
-     * intersecting Der Durchschnitt der beiden Rechtecke ist ein anderes Rechteck mit einer Fläche > 0.
-     *
-     * @param args gegenüber liegende Ecken (px,py), (qx,qy) bzw. (sx,sy), (tx,ty) von zwei Rechtecken
-     */
+
     public static void main(final String[] args) {
         int a = 0;
         final int px = Integer.parseInt(args[a++]);
